d1406cfc74ea261cd1c58555f7110d00ba369423
RoshanZedexInfo <roshan.moolya@zedexinfo.com>
2022-06-29 15:57:40 +0530

    testing

---
+++ js/global.js
@@ -0,0 +1,8 @@
+window.onload = function(){
+    //vanilla Lazy loading init
+    var lazyLoadInstance = new LazyLoad({
+        // Your custom settings go here
+    });
+
+    lazyLoadInstance.update();
+}
