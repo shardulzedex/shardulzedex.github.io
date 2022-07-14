dff23623ea8687109409be40609f0f4241d1aaae
RoshanZedexInfo <roshan.moolya@zedexinfo.com>
2022-07-14 14:25:06 +0530

    update code

--- js/home.js
+++ js/home.js
@@ -1,20 +1,12 @@
 
-    //   var swiper = new Swiper(".mySwiper", {
-    //     spaceBetween: 30,
-    //     centeredSlides: true,
-    //     autoplay: {
-    //       delay: 2500,
-    //       disableOnInteraction: false,
-    //     },
-    //     pagination: {
-    //       el: ".swiper-pagination",
-    //       clickable: true,
-    //     },
-    //     navigation: {
-    //       nextEl: ".swiper-button-next",
-    //       prevEl: ".swiper-button-prev",
-    //     },
-    //   });
+     var swiper = new Swiper(".mySwiper", {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
 
 
 
